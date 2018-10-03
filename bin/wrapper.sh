e7e7f71bafc55681da717239a0c192e4d503880d
Terence Sambo <arubislander@zonnet.nl>
2018-10-04 01:35:04 +0200

    Added configuration properties for port, http-port, max-memory, context path and host

--- bin/wrapper.sh
+++ bin/wrapper.sh
@@ -14,6 +14,13 @@ export SUBSONIC_HOME=$SNAP_USER_COMMON/var/subsonic
 export SUBSONIC_DEFAULT_MUSIC_FOLDER=$SNAP_USER_COMMON/var/music
 export SUBSONIC_DEFAULT_PODCAST_FOLDER=$SNAP_USER_COMMON/var/music/Podcast
 export SUBSONIC_DEFAULT_PLAYLIST_FOLDER=$SNAP_USER_COMMON/var/playlists
+
+export SUBSONIC_HOST="$(snapctl get host)"    # ${SUBSONIC_HOST:-0.0.0.0}
+export SUBSONIC_PORT="$(snapctl get port)"    # ${SUBSONIC_PORT:-4040}
+export SUBSONIC_HTTPS_PORT="$(snapctl get https-port)"    # ${SUBSONIC_HTTPS_PORT:-0}
+export SUBSONIC_CONTEXT_PATH="$(snapctl get context-path)"    # ${SUBSONIC_CONTEXT_PATH:-/}
+export SUBSONIC_MAX_MEMORY="$(snapctl get max-memory)"    # ${SUBSONIC_MAX_MEMORY:-150}
+
 export PATH=$SNAP/usr/lib/jvm/java-8-openjdk-$SNAP_ARCH/bin:$PATH
 
 $SNAP/subsonic.sh
